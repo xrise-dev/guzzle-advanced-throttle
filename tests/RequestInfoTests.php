4ab40f089a4cb43a92ccfe2cba869fed1e44e6d9
Timo Prüße <chroma91@gmail.com>
2018-01-29 00:13:23 +0100

    RequestInfo Tests

---
+++ tests/RequestInfoTests.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace hamburgscleanest\GuzzleAdvancedThrottle\Tests;
+
+use DateTime;
+use hamburgscleanest\GuzzleAdvancedThrottle\RequestInfo;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class RequestInfoTests
+ * @package hamburgscleanest\GuzzleAdvancedThrottle\Tests
+ */
+class RequestInfoTests extends TestCase
+{
+
+    /** @test
+     */
+    public function can_be_created_statically()
+    {
+        $timestamp = (new DateTime())->getTimestamp();
+        $requestCount = 15;
+        $remainingSeconds = 60;
+        $requestInfo = RequestInfo::create($requestCount, $timestamp, $remainingSeconds);
+
+        $this->assertEquals($requestCount, $requestInfo->requestCount);
+        $this->assertEquals($remainingSeconds, $requestInfo->remainingSeconds);
+        $this->assertEquals($timestamp, $requestInfo->expiresAt->getTimestamp());
+    }
+}
